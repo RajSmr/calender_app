647c78e15c07dcc140395fad891d0e0a709f5c36
RajSmr <malligarajsampathkumar@gmail.com>
2022-02-03 14:31:19 +0530

    Admin changes

--- calender_app/events/models.py
+++ calender_app/events/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User as users
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=122)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=122)
-    phone = models.CharField('Contact Phone', max_length=122)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=122, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name
@@ -27,7 +27,7 @@ class Event(models.Model):
     event_date = models.DateTimeField('Event Date', )
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
     #venue = models.CharField(max_length=122)
-    manager = models.CharField(max_length=100)
+    manager = models.ForeignKey(users, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank = True)
     attendees = models.ManyToManyField(User, blank= True)
 
