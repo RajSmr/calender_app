17066c5601fa772193a6855c075664009930fda6
RajSmr <malligarajsampathkumar@gmail.com>
2022-02-05 11:27:21 +0530

    Event Update

--- calender_app/events/urls.py
+++ calender_app/events/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('show_venue/<venue_id>', views.show_venue, name="show_venue"),
     path('search_venues', views.search_venues, name="search_venues"),
     path('update_venue/<venue_id>', views.update_venue, name="update_venue"),
+    path('update_event/<event_id>', views.update_event, name="update_event"),
     path('add_event', views.add_event, name="add_event"),
 ]
