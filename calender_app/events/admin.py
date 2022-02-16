17645ea90c2ef15bc2290654bf16f1ab51f330e3
RajSmr <malligarajsampathkumar@gmail.com>
2022-02-16 14:47:20 +0530

    Admin Group section

--- calender_app/events/admin.py
+++ calender_app/events/admin.py
@@ -1,12 +1,18 @@
 from dataclasses import field
+from email.headerregistry import Group
 from re import search
 from django.contrib import admin
 from .models import Venue, User, Event
+from django.contrib.auth.models import Group
 
 # Adding our models
 #admin.site.register(Venue)
 #admin.site.register(Event)
 admin.site.register(User)
+
+# Remove Groups
+#admin.site.unregister(Group)
+
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
